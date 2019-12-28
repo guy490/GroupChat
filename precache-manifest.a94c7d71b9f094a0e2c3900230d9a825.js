9a13c5d1adbaaf23b9d4ba97abd2eb482eb3115c
guy490 <guy8824@gmail.com>
2019-12-28 19:21:59 +0200

    Updates

---
+++ precache-manifest.a94c7d71b9f094a0e2c3900230d9a825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd9257dd36e3a6b41a798a36f857b83",
+    "url": "/GroupChat/index.html"
+  },
+  {
+    "revision": "aac32b97ef1c2957c1f1",
+    "url": "/GroupChat/static/css/main.e213e1c0.chunk.css"
+  },
+  {
+    "revision": "edd1efe65dd6b2ba3536",
+    "url": "/GroupChat/static/js/2.4aca1204.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/GroupChat/static/js/2.4aca1204.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aac32b97ef1c2957c1f1",
+    "url": "/GroupChat/static/js/main.aaa750e3.chunk.js"
+  },
+  {
+    "revision": "2be3d2368cc718a0fdaf",
+    "url": "/GroupChat/static/js/runtime-main.97d952dd.js"
+  }
+]);
