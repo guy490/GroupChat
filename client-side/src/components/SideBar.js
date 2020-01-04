699a0806e6092ae33953eacdcb04abd8a1be11e9
guy490 <guy8824@gmail.com>
2020-01-04 17:18:46 +0200

    Fix rendered list

--- client-side/src/components/SideBar.js
+++ client-side/src/components/SideBar.js
@@ -1,22 +1,26 @@
 import "../styles/SideBar.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { socket } from "../client_socket";
 
 const SideBar = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    socket.on("listOfUsersHasChanged", userList => {
+      setUserList(userList);
+    });
+  }, []);
+
+  const renderUserList = () => {
+    return userList.map(user => {
+      return <button className="logged-usr">Guest {user.username}</button>;
+    });
+  };
+
   return (
     <div className="ui sideBar segment">
       <button className="header">Current Users:</button>
-      <button className="logged-usr">User 1</button>
-      <button className="logged-usr">User 2</button>
-      <button className="logged-usr">User 3</button>
-      <button className="logged-usr">User 4</button>
-      <button className="logged-usr">User 5</button>
-      <button className="logged-usr">User 6</button>
-      <button className="logged-usr">User 7</button>
-      <button className="logged-usr">User 8</button>
-      <button className="logged-usr">User 9</button>
-      <button className="logged-usr">User 10</button>
-      <button className="logged-usr">User 11</button>
-      <button className="logged-usr">User 12</button>
+      {renderUserList()}
     </div>
   );
 };
