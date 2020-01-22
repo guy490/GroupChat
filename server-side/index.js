150cf48f0bb5631f3924adb7c2fd8e6f5d22c475
guy490 <guy8824@gmail.com>
2020-01-22 23:51:54 +0200

    Server name issue - fixed

--- server-side/index.js
+++ server-side/index.js
@@ -33,6 +33,6 @@ io.on("connection", client => {
   });
 });
 
-const port = /*process.env.PORT ||*/ 8000;
+const port = process.env.PORT || 8000;
 io.listen(port);
 console.log("listening on port ", port);
